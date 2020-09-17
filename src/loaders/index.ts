ed74e74da81cb5fa242f16a0e5992aac5c7f9bb8
Rami Abdou <ramiabdou98@gmail.com>
2020-09-17 15:11:04 -0700

    Update the index loader.

--- src/loaders/index.ts
+++ src/loaders/index.ts
@@ -1,18 +1,26 @@
 /**
- * @fileoverview - Calls all loader functions.
+ * @fileoverview Loader:  Calls all loader functions.
  * @author Rami Abdou
  */
 
 import './misc';
 
-import { Application } from 'express';
-
+import { APP } from '@constants';
 import { createConnection } from '@util/db/util';
-import startApolloServer from './apollo';
-import express from './express';
+import startApollo from './apollo';
+import startExpress from './express';
 
-export default async (app: Application) => {
-  const apolloServer = await startApolloServer();
-  apolloServer.applyMiddleware({ app: express(app), path: '/graphql' });
-  await createConnection();
+/**
+ * The entry point of the application. Runs all of the loaders functions
+ * including starting the Express and Apollo (GraphQL) servers and making the
+ * PostgreSQL database connections via MikroORM.
+ */
+
+const startServer = async () => {
+  const app = startExpress();
+  const [apolloServer] = await Promise.all([startApollo(), createConnection()]);
+  apolloServer.applyMiddleware({ app, path: '/graphql' });
+  app.listen(APP.PORT);
 };
+
+startServer();
