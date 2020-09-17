cae3678a2a0f31ebadbfe1a5ef892674a5db7d56
Rami Abdou <ramiabdou98@gmail.com>
2020-09-17 15:18:46 -0700

    Simplified the loader files.

--- src/loaders/index.ts
+++ src/loaders/index.ts
@@ -1,20 +1,16 @@
 /**
- * @fileoverview Loader:  Calls all loader functions.
+ * @fileoverview Loader: Entry Point
+ * - The entry point of the application. Runs all of the loaders functions
+ * including starting the Express and Apollo (GraphQL) servers and making the
+ * PostgreSQL database connections via MikroORM. Also loads files that otherwise
+ * wouldn't be loaded so events can be triggered and fired correctly.
  * @author Rami Abdou
  */
-
-import './misc';
 
 import { APP } from '@constants';
 import { createConnection } from '@util/db/util';
 import startApollo from './apollo';
 import startExpress from './express';
-
-/**
- * The entry point of the application. Runs all of the loaders functions
- * including starting the Express and Apollo (GraphQL) servers and making the
- * PostgreSQL database connections via MikroORM.
- */
 
 const startServer = async () => {
   const app = startExpress();
