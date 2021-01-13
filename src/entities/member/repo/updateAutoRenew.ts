53e627a31013469915f542631ef00f586c1ea864
Rami Abdou <ramiabdou98@gmail.com>
2021-01-12 17:19:15 -0800

    Need to figure out how auto-renewal works.

--- src/entities/member/repo/updateAutoRenew.ts
+++ src/entities/member/repo/updateAutoRenew.ts
@@ -1,9 +1,9 @@
-import { nanoid } from 'nanoid';
+// import { nanoid } from 'nanoid';
 import { ArgsType, Field } from 'type-graphql';
 
 import { GQLContext } from '@constants';
 import BloomManager from '@core/db/BloomManager';
-import { stripe } from '@integrations/stripe/Stripe.util';
+// import { stripe } from '@integrations/stripe/Stripe.util';
 import Member from '../Member';
 
 @ArgsType()
@@ -23,18 +23,18 @@ const updateAutoRenew = async (
     { event: 'AUTO_RENEW_UPDATED', populate: ['community.integrations'] }
   );
 
-  const { stripeSubscriptionId } = member;
+  // const { stripeSubscriptionId } = member;
 
-  if (stripeSubscriptionId) {
-    await stripe.subscriptions.update(
-      stripeSubscriptionId,
-      { collection_method: status ? 'charge_automatically' : 'send_invoice' },
-      {
-        idempotencyKey: nanoid(),
-        stripeAccount: member.community.integrations.stripeAccountId
-      }
-    );
-  }
+  // if (stripeSubscriptionId) {
+  //   await stripe.subscriptions.update(
+  //     stripeSubscriptionId,
+  //     { collection_method: status ? 'charge_automatically' : 'send_invoice' },
+  //     {
+  //       idempotencyKey: nanoid(),
+  //       stripeAccount: member.community.integrations.stripeAccountId
+  //     }
+  //   );
+  // }
 
   return member;
 };
