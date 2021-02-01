9ded22ad87c786247eb2a2b5402507dcc547516b
Rami Abdou <ramiabdou98@gmail.com>
2021-02-01 07:14:17 -0800

    Nice documentation.

--- src/entities/event-watch/repo/createEventWatch.ts
+++ src/entities/event-watch/repo/createEventWatch.ts
@@ -10,6 +10,15 @@ export class CreateEventWatchArgs {
   eventId: string;
 }
 
+/**
+ * Returns a new EventWatch.
+ *
+ * Invalidates QueryEvent.GET_EVENT and QueryEvent.GET_PAST_EVENTS.
+ *
+ * @param args.eventId - Identifier of the event.
+ * @param ctx.communityId - Identifier of the community.
+ * @param ctx.memberId - Identifier of the member.
+ */
 const createEventWatch = async (
   { eventId }: CreateEventWatchArgs,
   { communityId, memberId }: Pick<GQLContext, 'communityId' | 'memberId'>
