b7b7e30faa5d28bdbe0ddef60747164cf23af450
Jehron Petty <jehronp@gmail.com>
2021-03-31 17:19:45 -0400

    Implementation for creating a new question

---
+++ src/entities/question/repo/createQuestion.ts
@@ -0,0 +1,56 @@
+import { ArgsType, Field } from 'type-graphql';
+
+import BloomManager from '@core/db/BloomManager';
+import { GQLContext } from '@util/constants';
+import Question, { QuestionCategory, QuestionType } from '../Question';
+
+@ArgsType()
+export class CreateQuestionArgs {
+  @Field()
+  title: string;
+
+  @Field(() => String, { nullable: true })
+  category?: QuestionCategory;
+
+  @Field({ nullable: true })
+  description?: string;
+
+  @Field({ defaultValue: false })
+  locked: boolean = false;
+
+  @Field(() => [String], { nullable: true })
+  options?: string[];
+
+  @Field({ nullable: true })
+  rank?: number;
+
+  @Field()
+  required: boolean = true;
+
+  @Field(() => String, { nullable: true })
+  type: QuestionType;
+}
+
+/**
+ * Returns a new Question.
+ *
+ * @param args - Question data (eg: description, endTime, startTime).
+ * @param ctx.communityId - ID of the Community (authenticated).
+ * @param ctx.memberId - ID of the Member (authenticated).
+ */
+const createQuestion = async (
+  args: CreateQuestionArgs,
+  ctx: Pick<GQLContext, 'communityId'>
+): Promise<Question> => {
+  const { communityId } = ctx;
+  const { ...questionData } = args;
+
+  const question: Question = await new BloomManager().createAndFlush(Question, {
+    ...questionData,
+    community: communityId
+  });
+
+  return question;
+};
+
+export default createQuestion;
