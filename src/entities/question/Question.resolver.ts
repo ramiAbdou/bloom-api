c2d1899695086703e29bdfcefc77fe7c312f22f8
Jehron Petty <jehronp@gmail.com>
2021-03-31 17:45:35 -0400

    Updated Question resolver to include new GraphQL functions

--- src/entities/question/Question.resolver.ts
+++ src/entities/question/Question.resolver.ts
@@ -3,6 +3,8 @@ import { Args, Authorized, Ctx, Mutation, Query, Resolver } from 'type-graphql';
 import { MemberRole } from '@entities/member/Member';
 import { GQLContext } from '@util/constants';
 import Question from './Question';
+import createQuestion, { CreateQuestionArgs } from './repo/createQuestion';
+import deleteQuestion, { DeleteQuestionArgs } from './repo/deleteQuestion';
 import listQuestions from './repo/listQuestions';
 import updateQuestion, { UpdateQuestionArgs } from './repo/updateQuestion';
 
@@ -11,6 +13,21 @@ export default class QuestionResolver {
   @Query(() => [Question])
   async listQuestions(@Ctx() ctx: GQLContext): Promise<Question[]> {
     return listQuestions(ctx);
+  }
+
+  @Authorized(MemberRole.ADMIN)
+  @Mutation(() => Question, { nullable: true })
+  async createQuestion(
+    @Args() args: CreateQuestionArgs,
+    @Ctx() ctx: GQLContext
+  ): Promise<Question> {
+    return createQuestion(args, ctx);
+  }
+
+  @Authorized(MemberRole.ADMIN)
+  @Mutation(() => Question)
+  async deleteQuestion(@Args() args: DeleteQuestionArgs): Promise<Question> {
+    return deleteQuestion(args);
   }
 
   @Authorized(MemberRole.ADMIN)
